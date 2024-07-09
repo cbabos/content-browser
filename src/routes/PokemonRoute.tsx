7da46e0e8614f4af8e2aba591aabd9e588354350
Babos Csaba <babos.csaba@yourweb.hu>
2024-07-09 09:39:48 +0200

    Added basic csv export

--- src/routes/PokemonRoute.tsx
+++ src/routes/PokemonRoute.tsx
@@ -2,17 +2,17 @@ import { useLoaderData, useNavigate } from 'react-router-dom';
 import { tPokemon } from '../typedefs/ItemDescriptor';
 
 const PokemonRoute = () => {
-    const pokemon : tPokemon = useLoaderData() as tPokemon;
+    const pokemon: tPokemon = useLoaderData() as tPokemon;
     const navigate = useNavigate();
     const onBack = () => {
         navigate(-1);
     }
-    
+
     return (
         <div className="detailView">
-           <h1>{pokemon.name}</h1>
+            <h1>{pokemon.name}</h1>
             <button onClick={onBack}>Back</button>
-            <pre>{JSON.stringify(pokemon, null, " ")}</pre>
+            <code>{JSON.stringify(pokemon, null, " ")}</code>
         </div>
     );
 };
